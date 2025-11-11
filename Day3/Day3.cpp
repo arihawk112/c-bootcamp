b746ab5be7363067653de2f51cfdecd69975eb91
Arion A <Arion A@ARION>
2025-11-11 10:35:17 -0500

    Added methods stateChanger and StringHelper for deviceState output

--- Day3/Day3.cpp
+++ Day3/Day3.cpp
@@ -13,21 +13,47 @@ struct Sensor {
     int reading;
     DeviceState state;
 };
+//method to change any reading sensor to active state rather than idle, input not limited to 3 
+void stateChanger(Sensor& s)
+{
+        if (s.reading > 0)
+        {
+            s.state = DeviceState::Active;
+        }
+        else 
+        {
+            s.state = DeviceState::Error;
+        }
+}
+
+//DeviceState to string conversion fuction
+std::string toString(DeviceState state)
+{
+    switch (state) {
+    case DeviceState::Idle:   return "Idle";
+    case DeviceState::Active: return "Active";
+    case DeviceState::Error:  return "Error";
+    default:                  return "Unknown";
+	}
+}
+
 
 void printSensor(const Sensor& s) {
+	std::string statetemp = toString(s.state);
     std::cout << "Sensor: " << s.name
         << " | Reading: " << s.reading
-        << " | State: ";
+        << " | State: " << statetemp;
 
-    switch (s.state) {
+/*    switch (s.state) {
     case DeviceState::Idle:   std::cout << "Idle"; break;
     case DeviceState::Active: std::cout << "Active"; break;
     case DeviceState::Error:  std::cout << "Error"; break;
     }
-
+            
+    */
     std::cout << std::endl;
 }
-
+//added method to count sensors in Error state
 int countErrors(const std::array<Sensor, 3>& sensors)
 {
 	int errorCount = 0;
@@ -49,11 +75,20 @@ int main() {
 
     for (const auto& s : sensors) {
         printSensor(s);
-
     }
 
     int errorCount = countErrors(sensors);  
 
     std::cout << "Error Count: " << errorCount << std::endl;
+
+
+    //call state changer method for each sensor 
+    for (auto& s : sensors) {
+        stateChanger(s);
+    }
+
+    for (const auto& s : sensors) {
+        printSensor(s);
+    }
 
 }
